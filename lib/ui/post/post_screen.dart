ceac625abcb4e09dac10f309821abc1206badc9c
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-03-09 21:14:19 +0700

    Buat fitur refresh di List Posts

--- lib/ui/post/post_screen.dart
+++ lib/ui/post/post_screen.dart
@@ -9,11 +9,27 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  List<PostData> _postDatas = List<PostData>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Posts"),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(
+                Icons.refresh,
+                semanticLabel: "Refresh",
+              ),
+            ),
+          ),
+        ],
       ),
       body: SafeArea(
         child: Container(
@@ -22,43 +38,11 @@ class _PostScreenState extends State<PostScreen> {
             future: getAllPosts(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
-                return ListView.builder(
-                  itemBuilder: (context, index) {
-                    var postData = snapshot.data[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 4.0),
-                      child: Card(
-                        child: InkWell(
-                          onTap: () {
-                            // TODO: do something in here
-                          },
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 16.0, left: 16.0),
-                                child: Text(
-                                  postData.title,
-                                  style: Theme.of(context).textTheme.title,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    bottom: 16.0, left: 16.0),
-                                child: Text(
-                                  postData.author,
-                                  style: Theme.of(context).textTheme.body1,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                  itemCount: snapshot.data.length,
-                );
+                _postDatas.clear();
+                snapshot.data.forEach((postDataResponse) {
+                  _postDatas.add(postDataResponse);
+                });
+                return _buildListView(_postDatas);
               } else {
                 return Center(
                   child: CircularProgressIndicator(),
@@ -84,6 +68,44 @@ class _PostScreenState extends State<PostScreen> {
           );
         },
       ),
+    );
+  }
+
+  Widget _buildListView(List<PostData> postDatas) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        var postData = _postDatas[index];
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 4.0),
+          child: Card(
+            child: InkWell(
+              onTap: () {
+                // TODO: do something in here
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+                    child: Text(
+                      postData.title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0, left: 16.0),
+                    child: Text(
+                      postData.author,
+                      style: Theme.of(context).textTheme.body1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      itemCount: _postDatas.length,
     );
   }
 }
